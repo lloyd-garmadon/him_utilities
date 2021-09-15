06a9a06c55f09a4d6078ae14e7287f973fda9c54
Sven Himstedt <sven.himstedt@gmx.de>
2021-09-15 19:00:43 +0200

    first try

---
+++ src/him_utilities.h
@@ -0,0 +1,42 @@
+/***********************************************************************
+* FILENAME:
+*       him_utilities.h
+*
+* DESCRIPTION:
+*       Him Arduino helpful defines
+*
+* NOTES:
+*       This library was written to design just for fun.
+*
+* AUTHOR:
+*       Sven Himstedt
+*
+* COPYRIGHT: 
+*       Sven Himstedt 2021, All rights reserved.
+*
+* LICENSE:
+*       This library is free software.
+*       You can redistribute it and/or modify it.
+*       But give credit or you are an asshole.
+*
+* START DATE:
+*       2021/13/09
+*
+* CHANGES:
+*
+***********************************************************************/
+
+#ifndef _HIM_UTILITIES_H_
+#define _HIM_UTILITIES_H_
+
+
+
+#define STRINGIFY_IDENT(str) #str
+#define STRINGIFY_MACRO(str) STRINGIFY_IDENT(str)
+
+#define PROJECT_NAME_STRING     STRINGIFY_MACRO(PROJECT_NAME)
+#define PROJECT_VERSION_STRING  STRINGIFY_MACRO(PROJECT_VERSION_MAJOR) "." STRINGIFY_MACRO(PROJECT_VERSION_MINOR) "." STRINGIFY_MACRO(PROJECT_VERSION_PATCH)
+
+
+
+#endif /* _HIM_UTILITIES_H_ */
